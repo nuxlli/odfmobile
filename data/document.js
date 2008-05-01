8c957ce69ebcbdd0b145b963da0f43da0e4e5afa
Everton Ribeiro <nuxlli@localhost.localdomain>
2008-05-01 12:33:58 -0300

    Add licence

--- data/document.js
+++ data/document.js
@@ -1,3 +1,30 @@
+
+/*##############################################################################
+# ODF Mobile
+#
+# License: GPL
+# Copyright (c) 2008 ODF Mobile team
+#
+#
+# Last Modified: 2008-05-1 12:24
+#
+# License Information:
+#
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# at your option.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+##############################################################################*/
+
 function sleep(millisecondi) {
   var now = new Date();
   var exitTime = now.getTime() + millisecondi;
